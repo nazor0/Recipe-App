3bcfa61d2d7d01abfe5586f7a2b19f86d596a900
Don Belen <erbelen12@gmail.com>
2024-04-06 11:30:11 -0700

    Stuck with mongo

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,3 +1,6 @@
+require ('../models/database');
+const category = require('../models/category');
+
 /**
  * GET /
  * Homepage
@@ -8,3 +11,47 @@ exports.homepage = async (req, res) => {
   res.render('index', {title: ''});
 
 }
+
+async function insertDymmyCategoryData (){
+try{
+
+  await category.insertMany( [ 
+    {
+      name: "String",
+    image: "thai-food.jpg"
+  },
+
+  {
+    name: "String",
+    image: "thai-food.jpg"
+  },
+
+  {
+    name: "String",
+    image: "thai-food.jpg"
+  },
+
+  {
+    name: "String",
+    image: "thai-food.jpg"
+  },
+
+  {
+    name: "String",
+    image: "thai-food.jpg"
+  },
+  
+  {
+    name: "String",
+    image: "thai-food.jpg"
+  },
+
+  ]);
+
+} catch(error){
+  console.log('err', + error)
+
+  } 
+}
+
+insertDymmyCategoryData();
