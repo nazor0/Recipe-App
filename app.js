295bb43245b278c6e3e7c715e7e155d206019c90
Serjeoh Nakata <serjnak@gmail.com>
2024-04-09 15:34:10 -0700

    v2.1 - restyled the search pages

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require ('express');
 const expressLayouts = require ('express-ejs-layouts');
 
-
 const app = express();
 const port = process.env.PORT || 3000;
-const key_api = "9c191e5eb0c34564a3c3562ca8084563";
 
 require ('dotenv').config();
 app.use(express.urlencoded( {extended: false} ));
